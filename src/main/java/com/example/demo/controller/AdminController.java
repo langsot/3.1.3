0482342aad0092eaccb1ed364ca55e549daae454
Eugene Belezki <langsot1337@gmail.com>
2021-12-19 22:34:18 +0700

    commit just for commit

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -4,6 +4,7 @@ import com.example.demo.models.User;
 import com.example.demo.service.RoleService;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
@@ -22,44 +23,46 @@ public class AdminController {
     }
 
     @GetMapping()
-    public String allUser(ModelMap model) {
-        model.addAttribute("user", service.getAll());
+    public String allUser(@AuthenticationPrincipal User user, ModelMap model) {
+        model.addAttribute("user", user);
+        model.addAttribute("allUser", service.getAll());
+        model.addAttribute("allRole", role.getAll());
         return "/admin";
     }
 
-    @GetMapping(value = "/add")
-    public String newUser(ModelMap model) {
-        model.addAttribute("newU", new User());
-        model.addAttribute("role", role.getAll());
-        return "/create";
-    }
+//    @GetMapping(value = "/add")
+//    public String newUser(ModelMap model) {
+//        model.addAttribute("newUser", new User());
+//        model.addAttribute("role", role.getAll());
+//        return "/create";
+//    }
 
-    @PostMapping()
-    public String create(@ModelAttribute("newU") User user, @RequestParam(value = "role") String[] nameRole) {
+    @PostMapping("/add")
+    public String create(@ModelAttribute("newUser") User user, @RequestParam(value = "role") String[] nameRole) {
         //тут мы должны ловить искл если роли не выбраны (так же и в эдите)
         user.setRoles(role.setOfRole(nameRole));
         service.add(user);
         return "redirect:/admin";
     }
 
-    @GetMapping(value = "/edit/{id}")
-    public String editUser(@PathVariable("id") Long id, ModelMap model) {
-        model.addAttribute("userEdit", service.userById(id));
-        model.addAttribute("role", role.getAll());
-        return "/edit";
-    }
+//    @GetMapping(value = "/edit/{id}")
+//    public String editUser(@PathVariable("id") Long id, ModelMap model) {
+//        model.addAttribute("userEdit", service.userById(id));
+//        model.addAttribute("role", role.getAll());
+//        return "/edit";
+//    }
 
-    @PutMapping("/{id}")
-    public String refactor(@ModelAttribute("userEdit") User user,
+    @PutMapping("/edit")
+    public String refactor(@ModelAttribute User user,
                            @RequestParam(value = "role") String[] nameRole) {
         user.setRoles(role.setOfRole(nameRole));
         service.edit(user);
         return "redirect:/admin";
     }
 
-    @DeleteMapping("/{id}")
-    public String deleteUser(@PathVariable("id") Long id) {
-        service.delete(id);
+    @DeleteMapping("/delete")
+    public String deleteUser(@ModelAttribute User user) {
+        service.delete(user.getId());
         return "redirect:/admin";
     }
 
